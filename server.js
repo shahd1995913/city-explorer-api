dedd4c2b88ae28ac0570bbc9a82d69f85190d481
shahd1995913 <shahdalkhatib95@gmail.com>
2021-09-16 05:12:32 +0300

    update1

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+const express =require('express');
+
+const server =express();
+
+
+const PORT =3010;
+
+
+
+server.listen(PORT,()=>{
+
+console.log(`Hello , I am Listening to the Port Number is ${PORT}`)
+
+
+})
